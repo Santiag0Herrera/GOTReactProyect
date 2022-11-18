e0795204e25fef881e7e9d7c9a4358e6be1ef30a
AnabelAramayo <anabelaramayo@gmail.com>
2022-11-18 17:27:40 -0300

    fetch en app de api got

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <>
-      <h1>New App!</h1>
-    </>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://thronesapi.com/api/v2/Characters")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.firstName}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default App;
